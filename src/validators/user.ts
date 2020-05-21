9c0e38e1272042db6729c048ef56d1c57b59e9f4
jose.luisdiaz <jose.luisdiaz@yello.co>
2020-05-21 16:04:10 -0500

    add phone validation

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -1,15 +1,37 @@
 const Joi = require('@hapi/joi');
+const Phonenumber = require('awesome-phonenumber');
 
 const ValidRoles = ['Player', 'Admin'];
 const PasswordRegex = new RegExp('^[a-zA-Z0-9]{3,30}$');
 
 const RequiredString = Joi.string().required();
 
+const validatePhone = (value, helpers) => {
+  const {
+    state: {
+      ancestors: [input]
+    }
+  } = helpers;
+  const phoneNumber = Phonenumber(value, input.countryCode);
+
+  if (phoneNumber.isValid()) {
+    return phoneNumber.getNumber('international');
+  }
+
+  return helpers.error('any.invalid');
+}
+
 const createSchema = Joi.object({
   firstName: RequiredString,
   lastName: RequiredString,
   birthday: Joi.date(),
-  phone: RequiredString,
+  phone: RequiredString
+    .custom(validatePhone, 'Phone validation')
+    .messages({'any.invalid': 'Phone Number or Country Code invalid'}),
+  countryCode: RequiredString
+    .min(2)
+    .max(2)
+    .uppercase(),
   email: RequiredString.email({multiple: false}),
   password: RequiredString.pattern(PasswordRegex),
   passwordConfirmation: Joi.ref('password'),
--- test/validators/user.test.ts
+++ test/validators/user.test.ts
@@ -21,6 +21,7 @@ const input = {
   password: 'idontcare',
   passwordConfirmation: 'idontcare',
   phone: '3213201312',
+  countryCode: 'co',
   birthday: '2/28/1969',
   role: 'Player'
 }
@@ -40,13 +41,14 @@ describe('createSchema', () => {
       lastName: 'Bing',
       password: 'idontcare',
       passwordConfirmation: 'idontcare',
-      phone: '3213201312'
+      phone: '+57 321 3201312'
+      countryCode: 'CO'
     };
     expect(value.birthday).to.be.a.date();
   })
 
   it('is not valid if email has wrong format', () => {
-    let newInput = {...input, email: 'hey'}
+    let newInput = { ...input, email: 'hey' }
     const { error, value } = createSchema.validate(newInput, options)
 
     expect(error.details).to.equal([
@@ -59,43 +61,88 @@ describe('createSchema', () => {
     ]);
   })
 
+  it('is not valid when passwords dont match', () => {
+    let newInput = { ...input, passwordConfirmation: 'whatthehell' }
+    const { error, value } = createSchema.validate(newInput, options)
+
+    expect(error.details[0]).to.include(
+      {
+        message: '"passwordConfirmation" must be [ref:password]',
+        path: ['passwordConfirmation'],
+        type: 'any.only',
+      }
+    );
+  })
+
+  it('is not valid when phone is not valid', () => {
+    let newInput = { ...input, phone: '0707123456' }
+    const { error, value } = createSchema.validate(newInput, options)
+
+    expect(error.details[0]).to.include(
+      {
+        message: 'Phone Number or Country Code invalid',
+        path: ['phone'],
+        type: 'any.invalid',
+      }
+    );
+  })
+
+  it('is not valid when countryCode is not valid', () => {
+    let newInput = { ...input, countryCode: 'SE' }
+    const { error, value } = createSchema.validate(newInput, options)
+
+    expect(error.details[0]).to.include(
+      {
+        message: 'Phone Number or Country Code invalid',
+        path: ['phone'],
+        type: 'any.invalid',
+      }
+    );
+  })
+
   it('is not valid when input is empty', () => {
     const { error, value } = createSchema.validate({}, options)
 
     const expectedErrors = [
       {
         message: '"firstName" is required',
-        path: [ 'firstName' ],
+        path: ['firstName'],
         type: 'any.required',
         context: { label: 'firstName', key: 'firstName' }
       },
       {
         message: '"lastName" is required',
-        path: [ 'lastName' ],
+        path: ['lastName'],
         type: 'any.required',
         context: { label: 'lastName', key: 'lastName' }
       },
       {
         message: '"phone" is required',
-        path: [ 'phone' ],
+        path: ['phone'],
         type: 'any.required',
         context: { label: 'phone', key: 'phone' }
       },
       {
+        message: '"countryCode" is required',
+        path: ['countryCode'],
+        type: 'any.required',
+        context: { label: 'countryCode', key: 'countryCode' }
+      },
+      {
         message: '"email" is required',
-        path: [ 'email' ],
+        path: ['email'],
         type: 'any.required',
         context: { label: 'email', key: 'email' }
       },
       {
         message: '"password" is required',
-        path: [ 'password' ],
+        path: ['password'],
         type: 'any.required',
         context: { label: 'password', key: 'password' }
       },
       {
         message: '"role" is required',
-        path: [ 'role' ],
+        path: ['role'],
         type: 'any.required',
         context: { label: 'role', key: 'role' }
       }
